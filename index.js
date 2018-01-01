ccd91582ba8358bc22b4cb0d39c5244e1fe7f091
vijaykumar51 <vijay.kumar.em51@gmail.com>
2018-01-01 22:41:47 +0530

    adding asset info and percentage

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
+const webpack = require("webpack");
+
 class WebpackAdminDashboard {
 	apply(compiler) {
+
 		compiler.plugin("watch-run", (compiler, callback) => {
-			console.log("===> watch");
+			console.log("\n===> watching");
 			if (callback) {
 				callback();
 			}
 		});
-		compiler.plugin("done", (stats, callback) => {
-			console.log("===> done");
-			if (callback) {
-				callback();
-			}
-		});
+
 		compiler.plugin("emit", (compilation, callback) => {
-			console.log("===> emit");
+			console.log("\n===> emitting files");
 			if (callback) {
 				callback();
 			}
 		});
+
+		compiler.plugin("done", (stats, callback) => {
+			console.log("\n===> complete");
+
+			var assets = stats.compilation.assets;
+			console.log(Object.keys(assets));
+			(Object.keys(assets)).forEach(asset => {
+				console.log("---------");
+				console.log(assets[asset]["_cachedSize"]);
+				console.log(assets[asset]["emitted"]);
+				console.log(assets[asset]["existsAt"]);
+			})
+
+			if (callback) {
+				callback();
+			}
+		});
+
+		compiler.apply(
+			new webpack.ProgressPlugin((percent, msg) => {
+				console.log("\npercent = ", percent * 100, "msg = ", msg);
+			})
+		);
 	}
 }
 
